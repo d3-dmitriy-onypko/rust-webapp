36699e1afe289c393674457966cfbbbcaf924ed8
Dmitriy Onypko <vaporif@gmail.com>
2022-07-13 00:08:06 +0100

    lazy_connections

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
+    let db_pool = PgPool::connect_lazy(&configuration.database.connection_string())
         .expect("failed to connetect to postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("failed to bind");
